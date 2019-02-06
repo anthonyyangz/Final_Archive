dd2a22a68b1eee9552c91cc4e9aa90b03b04c6aa
Anthony Yang <yzhao04@risd.edu>
2019-02-06 13:07:27 -0500

    This is Final Archive

---
+++ project2/main.js
@@ -0,0 +1,11 @@
+var blue =  [216, 91, 50];
+var red = [0, 100, 63];
+var yellow = [43, 96, 50];
+var green = [95, 100, 30];
+
+var color=[blue, red, yellow, blue, green, red];
+var myname="Google";
+drawName(myname, color);
+
+bubbleShape="square"
+bounceBubbles();
